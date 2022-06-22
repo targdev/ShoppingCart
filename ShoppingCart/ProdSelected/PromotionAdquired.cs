c34559d01145d490040bcb79aa5dc97cccd85cd0
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2022-06-22 13:40:53 -0300

    Add params the promotions

---
+++ ShoppingCart/ProdSelected/PromotionAdquired.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.ProdSelected
+{
+    internal class PromotionAdquired
+    {
+        public string PromotionByAmountCategories(int amountCategories)
+        {
+            switch (amountCategories)
+            {
+                case 2:
+                    return "DOUBLE LOOK";
+                case 3:
+                    return "TRIPLE LOOK";
+                case 4:
+                    return "FULL LOOK";
+                default:
+                    return "SINGLE LOOK";
+            }
+        }
+    }
+}
--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -16,6 +16,7 @@ namespace ShoppingCart
             var listProducts = JsonConvert.DeserializeObject<Root>(jsonStrg);
 
             DelimitersToSplit separators = new DelimitersToSplit();
+            PromotionAdquired promotionAdquired = new PromotionAdquired();
 
             Console.WriteLine("Quais itens deseja adicionar no carrinho?");
             var quantityItems = Console.ReadLine();
@@ -23,7 +24,26 @@ namespace ShoppingCart
 
             var selectProd = (from product in listProducts.Products
                               join id in resInputUser on product.Id equals id
-                             select product).ToList();
+                              select product).ToList();
+
+            var groupByCategoriesQuery = (from product in selectProd
+                                          group product by product.Category into newGroup
+                                          select newGroup).Count();
+
+           var discountClub = promotionAdquired.PromotionByAmountCategories(groupByCategoriesQuery);
+            var sumDiscounts = selectProd
+                .Aggregate(0d, (acc, product) => {
+                    var listadelista = product.Promotions.Where(promotion =>
+                    {
+                        var looks = promotion.Looks.Where(look => discountClub == look).FirstOrDefault();
+
+                        return looks != null;
+                    }).FirstOrDefault();
+                    //FAZER UMA COISA DAHORA COM O PRICE!!!!!!!!!!!!!
+                    var price = listadelista == null ? product.RegularPrice : listadelista.Price;
+
+                    return acc + price;
+                });
 
             Console.ReadKey();
         }
